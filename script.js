a41fe31a7064942142b2039e2f72866f7d93ec65
EJHacks <ee1912136@gmail.com>
2025-08-06 10:59:15 -0400

    added changes deleted comments

--- script.js
+++ script.js
@@ -6,16 +6,6 @@ const submitButton = document.getElementById("send-btn");
 
 
 submitButton.onclick = updateDB;
-/**
- * @TODO create a function called updateDB which takes
- * one parameter, the event, that:
- *      - gets the values of the input elements and stores
- *        the data in a temporary object with the keys USERNAME
- *        and MESSAGE
- *      - console.logs the object above
- *      - writes this object to the database
- *      - resets the value of #message input element
- */
 
 function updateDB(event) {
   event.preventDefault();
@@ -30,31 +20,10 @@ function updateDB(event) {
 
   message.value = ""
   username.value = ""
-  // Prevent default refresh
-  // Create data object
-  // console.log the object
-  // GET *PUSH* PUT DELETE
-  // Write to our database
-  // Reset message
 }
 
-/**
- * @TODO add the addMessageToBoard function as an event
- * handler for the "child_added" event on the database
- * object
- */
 database.on('child_added', addMessageToBoard);
 
-/**
- * @TODO create a function called addMessageToBoard that
- * takes one parameter rowData which:
- *      - console.logs the data within rowData
- *      - creates a new HTML element for a single message
- *        containing the appropriate data
- *      - appends this HTML to the div with id
- *        #all-messages (we should have a reference already!)
- *
- */
 
 function addMessageToBoard(rowData) {
   let data = rowData.val();
@@ -63,31 +32,8 @@ function addMessageToBoard(rowData) {
   let singleMessage = makeSingleMessageHTML(data.username, data.message);
   messagesDiv.appendChild(singleMessage);
 
-  // Store the value of rowData inside object named 'data'
-  // console.log data
-  // Create a variable named singleMessage
-  // that stores function call for makeSingleMessageHTML()
-  // Append the new message HTML element to allMessages
+
 }
-
-/**
- * @TODO create a function called makeSingleMessageHTML which takes
- * two parameters, usernameTxt and messageTxt, that:
- *      - creates a parent div with the class .single-message
- *
- *      - creates a p tag with the class .single-message-username
- *      - update the innerHTML of this p to be the username
- *        provided in the parameter object
- *      - appends this p tag to the parent div
- *
- *      - creates a p tag
- *      - updates the innerHTML of this p to be the message
- *        text provided in the parameter object
- *      - appends this p tag to the parent div
- *
- *      - returns the parent div
- */
-
 function makeSingleMessageHTML(usernameTxt, messageTxt) {
   let parentDiv = document.createElement("div");
   parentDiv.className = 'single-message';
