460078d1e3de21ae905103883c66469d11f4c211
EJHacks <ee1912136@gmail.com>
2025-08-06 11:41:40 -0400

    Changes Made to scripts

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const messagesDiv = document.getElementById("all-messages");
 const username = document.getElementById("username");
 const message = document.getElementById("message");
 const submitButton = document.getElementById("send-btn");
+const email = document.getElementById("email");
 
 
 submitButton.onclick = updateDB;
@@ -10,22 +11,23 @@ submitButton.onclick = updateDB;
 function updateDB(event) {
   event.preventDefault();
 
-  if(username.value == ""){
-    alert("Please insert a username!")
-  } else{
+  if (username.value == "") {
+    alert("Please insert a username!");
+    return; 
+  }
 
-  
   let data = {
     "username": username.value,
-    "message": message.value
-  }
-}
-  console.log(data)
+    "message": message.value,
+    "email":  email.value
+  };
 
-  database.push(data)
+  console.log(data);
+  database.push(data);
 
-  message.value = ""
-  username.value = ""
+  message.value = "";
+  username.value = "";
+  email.value = "";
 }
 
 database.on('child_added', addMessageToBoard);
@@ -35,24 +37,36 @@ function addMessageToBoard(rowData) {
   let data = rowData.val();
   console.log(data)
 
-  let singleMessage = makeSingleMessageHTML(data.username, data.message);
+  let singleMessage = makeSingleMessageHTML(data.username, data.message, data.email);
   messagesDiv.appendChild(singleMessage);
 
 
 }
-function makeSingleMessageHTML(usernameTxt, messageTxt) {
+function makeSingleMessageHTML(usernameTxt, messageTxt, emailTxt) {
   let parentDiv = document.createElement("div");
   parentDiv.className = 'single-message';
-  let usernameP = document.createElement("p");
-  usernameP.innerHTML = usernameTxt;
-  usernameP.className = 'single-message-username'
 
-  parentDiv.append(usernameP);
+  let usernameP = document.createElement("p");
+  usernameP.textContent = usernameTxt;
+  usernameP.className = 'single-message-username';
+
+  let emailP = document.createElement("p");
+  emailP.textContent = emailTxt;
+  emailP.className = 'single-message-email';
+
   let messageP = document.createElement("p");
-  messageP.innerHTML = messageTxt;
-  parentDiv.append(messageP);
+  messageP.textContent = messageTxt;
+
+  parentDiv.append(usernameP, messageP, emailP);
   return parentDiv;
 }
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const d = new Date();
+let month = months[d.getMonth()];
+
+
 
 /**
  * @BONUS add an onkeyup event handler to the form HTML
